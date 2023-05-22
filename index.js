fff51b91ed31faba0e18497fdb8890a425c4de1b
Ronopoldo <Ronopoldo@yandex.ru>
2023-05-22 22:24:30 +0300

    forward is workin

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // const fs = require('fs');
-const { Telegraf , Markup} = require('telegraf')
-const bot = new Telegraf()
-
+const { Telegraf , Markup, Telegram} = require('telegraf')
+const bot = new Telegraf('6114524938:AAFzgSOByjBvL8DrVLfOzIi0Z0BtRXPHpHE')
+const telegram = new Telegram('6114524938:AAFzgSOByjBvL8DrVLfOzIi0Z0BtRXPHpHE')
+// const Telegram = require('node-telegram-bot-api')
+// const bot1 = new Telegram('6114524938:AAFzgSOByjBvL8DrVLfOzIi0Z0BtRXPHpHE', { polling: true })
 console.log('READY')
 
 
@@ -13,16 +15,21 @@ async function forwarder(ctx, fwdID) {
     ctx.forwardMessage(fwdID)
     ctx.reply('^ ^ ^ ^', Markup.inlineKeyboard([{ text: 'AAA', callback_data: 'accept'}])
         .resize())
-    console.log(ctx.message)
+    // console.log(ctx.message)
 }
 
-bot.on('text', (ctx) =>
+bot.on('photo', (ctx) =>
 {
     forwarder(ctx, 925304597)
 })
 
 bot.action('accept', (ctx) => {
-    return console.log('xd')
+
+    // let tempCTX = new context()
+    telegram.forwardMessage(-1001537684385, 925304597, ctx.update.callback_query.message.message_id - 1)
+
+    // ctx.update.callback_query.message.message_id
+    return console.log('DONED')
 })
 
 // bot.on('text', async ctx => {
